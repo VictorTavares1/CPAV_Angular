f43478dd5839222e42cb18831c4ba6a293fbb67c
victor tavares <victortavares649@gmail.com>
2026-04-02 10:49:28 +0100

    feat: navbar com Bootstrap e logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { SobreNos } from './pages/sobre-nos/sobre-nos';
+import { Servicos } from './pages/servicos/servicos';
+import { Noticias } from './pages/noticias/noticias';
+import { Eventos } from './pages/eventos/eventos';
+import { Contactos } from './pages/contactos/contactos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'sobre-nos', component: SobreNos },
+    { path: 'servicos', component: Servicos },
+    { path: 'noticias', component: Noticias },
+    { path: 'eventos', component: Eventos },
+    { path: 'contactos', component: Contactos },
+    { path: '**', redirectTo: '' }
+];
